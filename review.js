1f8a8926ef2f5acacca90ced62aae40aa96c02fc
mukhtorov <mukhtorov_1994@mail.ru>
2023-02-23 16:37:54 +0500

    navbar

---
+++ review.js
@@ -0,0 +1,13 @@
+// git init
+// git remote add origin URL
+
+// PUSH
+// git add .
+// git commit -m "..."
+// git push origin master
+
+// branch
+// git branch
+// git branch "Name"
+// git checkout "name"
+// git branch -m "name"
